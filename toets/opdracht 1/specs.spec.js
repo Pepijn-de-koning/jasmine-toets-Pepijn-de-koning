0d92b0f170e3eb68429ea13e245659b61ca0d82f
Pepijn-de-koning <54800145+Pepijn-de-koning@users.noreply.github.com>
2020-10-20 10:51:37 +0200

    opdracht 1

--- toets/opdracht 1/specs.spec.js
+++ toets/opdracht 1/specs.spec.js
@@ -1,26 +1,30 @@
 describe('Testje 1', function() {
     it('should have the correct title', function () {
-        expect(document.title).toBe('Voorbeeld toets');
+        expect(document.title).toBe('Verbeteringen');
         expect(document.charset).toBe('UTF-8');
     });
 });
 
 describe('Testje 2', function () {
     it('should test', function () {
-        expect(unitTest.item).toBe('voorbeeld');
-        it('should test', function () {
-            expect(unitTest.nummer).toEqual(3);
-        });
+        expect(unitTest.item).toBe('item');
     });
 });
 
 describe('Testje 3', function () {
+  it('should test', function () {
+      expect(unitTest.nummer).toEqual(2);
+  });
+});
+
+describe('Testje 4', function () {
     it('should test', function () {
         expect(unitTest.beschrijving).toBe('Beschrijving');
-        describe('Testje 4', function () {
-            it('should test', function () {
-                expect(unitTest.boodschap).toEqual('Hallo');
-            });
-        });
+  });
+});
+
+describe('Testje 5', function () {
+    it('should test', function () {
+        expect(unitTest.boodschap).toEqual('Hallo');
     });
 });
--- toets/opdracht 2/winkelwagen.js
+++ toets/opdracht 2/winkelwagen.js
@@ -1,5 +1,27 @@
+class Artikel
+{
+  constructor(item, aantal, maat){
+      let VoorRaad = this;
+      VoorRaad.artikel = item;
+      VoorRaad.voorraad = parseFloat(aantal);
+      VoorRaad.maat = maat;
+  }
 
+  showVoorraad(){
+      console.log(` kleur = ${this.artikel} , maat = ${this.maat}`);
+  }
 
+  verhoogVoorraadMet(optellenMulti){
+      let verhogerMulti = this;
+          verhogerMulti.optellenMulti = parseFloat(optellenMulti);
+          console.log((this.voorraad += verhogerMulti.optellenMulti));
+      }
+}
 
+let ArtikelVariabel = new Artikel("T-shirt classic rood", 1, "Small");
+let ArtikelVariabel2 = new Artikel("T-shirt classic wit", 2, "Medium");
+let ArtikelVariabel3 = new Artikel("T-shirt classic blauw", 3, ";Large");
 
+ArtikelVariabel.voorraad;
 
+ArtikelVariabel.showVoorraad();
